b93669c24bf99ed779aae51ba8196873de1f2a45
Matt Gibbs <mgibbs@slac.stanford.edu>
2022-07-19 07:54:09 -0700

    Bump version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name='subprocessca',
-	version='1.0.5',
+	version='1.0.6',
 	packages=['subprocessca'],
 	description='Python wrapper for syscalls to EPIC Channel Access utilities',
 	author='Patrick Pascual',
